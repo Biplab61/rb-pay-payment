f71d77796ffe3dfd3111b325415ea8ab85b23a12
Biplab Pati <89262168+Biplab61@users.noreply.github.com>
2022-07-16 23:45:36 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,15 +5,6 @@ const Footer: NextPage = () => {
   return (
     <footer className="footer">
       <h1 className="logo">R B Pay</h1>
-      <p className="slogan">
-        Shareable Payment&apos;s Link for{" "}
-        <img
-          src="/images/upi.svg"
-          className="upilogo"
-          width="40"
-          alt="Upi Icon"
-        />
-      </p>
       <p className="tpf">
 
       </p>
